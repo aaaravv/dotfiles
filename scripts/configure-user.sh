43cd02f34de507df73428385c1b906a7fa4cd8b7
Aarav619 <aaravmishra619@gmail.com>
2022-01-26 17:41:30 +0530

    Rm .themes/ dir

--- scripts/configure-user.sh
+++ scripts/configure-user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$EUID" -eq 0 ]
-    then echo "Do not run as root"
-        exit
+then echo "Do not run as root"
+    exit
 fi
 
 echo "======> Copying config files to respective dir..."
@@ -11,24 +11,24 @@ echo
 mkdir -p ${HOME}/.config
 
 cp -frv \
-	../.themes \
-    ../.vim \
-	../.vimrc \
-	../.zshrc \
-	${HOME}/
+../.themes \
+../.vim \
+../.vimrc \
+../.zshrc \
+${HOME}/
 
 cp -frv \
-    ../gtk-3.0 \
-	../micro \
-	../spicetify \
-	../starship.toml \
-	${HOME}/.config/
+../gtk-3.0 \
+../micro \
+../spicetify \
+../starship.toml \
+${HOME}/.config/
 
 
 echo -e "\n======> Installing Vim-Plugins"
 if [ ! -e ${HOME}/.vim/autoload/plug.vim ]; then
-  curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 echo -e "\n======> Installing ZSH-Plugins"
