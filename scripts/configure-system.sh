128d093d6d344de5be4565238f17006962e7490a
Aarav619 <aaravmishra619@gmail.com>
2022-02-10 14:38:37 +0530

    Add -fv args to cp & add mirrorlist config

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -7,14 +7,17 @@ fi
 
 # running commands to configure the system
 systemctl enable NetworkManager
-cp /sys-configs/main.conf /etc/bluetooth/
+
+# Bluetooth config
+cp -fv /sys-configs/main.conf /etc/bluetooth/
 
 # Set time-date
 timedatectl set-timezone 'Asia/Kolkata'
 timedatectl set-ntp true
 
 # Pacman config
-cp /sys-configs/pacman.conf /etc/
+cp -fv /sys-configs/pacman.conf /etc/
+cp -fv /sys-configs/mirrorlist /etc/pacman.d/
 
 mkinitcpio -p linux
 
