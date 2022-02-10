fb0e178307122bae2b60c97f6ca45cac568a0f60
Aarav619 <aaravmishra619@gmail.com>
2022-02-10 14:44:45 +0530

    Rem oh-my-zsh, add aur-packages & update Yay inst

--- scripts/install-aur-packages.sh
+++ scripts/install-aur-packages.sh
@@ -2,18 +2,26 @@
 
 echo -e "\n======> Installing Yay\n"
 
-cd /opt
-sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R $USER:$USER ./yay-git
+cd ${HOME}
+git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si --no-confirm
 
-echo -e "\n======> Installing AUR Packages\n"
+echo -e "\n======> Installing AUR Packages...\n"
 
-yay -S $(<pkg/aur-packages.txt)
+yay -Y --devel --combinedupgrade --batchinstall --save
+yay -S \
+	7-zip-bin \
+	google-chrome \
+	llama \
+	nerd-fonts-jetbrains-mono \
+	ponymix \
+	rtl8821au-dkms-git \
+	shell-color-scripts \
+	spicetify-cli \
+	spotify \
+	spotify-adblock \
+	ttf-font-logos \
+	visual-studio-code-bin
 
-echo -e "\n======> Installing oh-my-zsh\n"
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo -e "\n======> Completed Installing AUR Packages!\n"
