7353da55c42e36ce53abeec8c7f6a6a806529af4
Aarav619 <aaravmishra619@gmail.com>
2022-03-16 21:15:34 +0530

    rm clutter

--- scripts/install-aur-packages.sh
+++ scripts/install-aur-packages.sh
@@ -9,19 +9,18 @@ makepkg -si --no-confirm
 
 echo -e "\n======> Installing AUR Packages...\n"
 
-yay -Y --devel --combinedupgrade --batchinstall --save
+yay -Y --devel --combinedupgrade --batchinstall --nodiffmenu --save
 yay -S \
 	autotiling-rs.git \
 	7-zip-bin \
 	google-chrome \
-	llama \
 	nerd-fonts-jetbrains-mono \
 	ponymix \
 	rtl8821au-dkms-git \
-	shell-color-scripts \
 	spicetify-cli \
 	spotify \
 	spotify-adblock \
+	tor-browser \
 	ttf-font-logos \
 	ttf-material-design-icons \
 	visual-studio-code-bin
